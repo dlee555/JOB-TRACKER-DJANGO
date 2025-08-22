dc5766e23821eb39e6caf3a8ee0c8b6e6be56b97
dlee555 <yeong.minglee1999@outlook.com>
2025-08-21 20:54:38 -0700

    updated models

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Application(models.Model):
+    STATUS_CHOICES = [
+        ("applied","Applied"),
+        ("interview","Interview"),
+        ("offer","Offer"),
+        ("rejected","Rejected")
+    ]
+
+    user = models.ForeignKey(User,
+     on_delete=models.CASCADE, 
+     related_name="applications")
+
+    company = models.CharField(max_length = 255)
+    job_title = models.CharField(max_length = 255)
+    application_link = models.URLField(blank = True)
+    status = models.CharField(
+        max_length = 20, choices = STATUS_CHOICES, default = "applied")
+    date_applied = models.DateField()
+    notes = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ["-date_applied","-id"]
+
+    def __str__(self):
+        return f"{self.company} - {self.job_title}"
